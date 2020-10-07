6d3c6776dcdbb8405db8cbac94c4effb0c671e15
Franco Di Leo <dielo.francoj@gmail.com>
2020-10-07 20:46:43 -0300

    node inicio

---
+++ node/clase01/routes/alumnos.js
@@ -0,0 +1,6 @@
+const express = require("express");
+// VERBOS HTTP -> GET, POST, PUT , DELETE
+const router = express.Router(); // Detectar verbos http
+
+//Router.use() requires a middleware function but got a Object
+module.exports = router;
