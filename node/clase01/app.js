e8e28a27c5688d82102dc39271d1ef2f5fcb62bd
Franco Di Leo <dielo.francoj@gmail.com>
2020-10-07 21:59:53 -0300

    CLASE 1 node

--- node/clase01/app.js
+++ node/clase01/app.js
@@ -7,8 +7,8 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const alumnos = require("./routes/alumnos");
+const login = require("./routes/login");
 var app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/alumnos", alumnos);
-
+app.use("/login", login);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- node/clase01/routes/alumnos.js
+++ node/clase01/routes/alumnos.js
@@ -1,6 +1,29 @@
 const express = require("express");
 // VERBOS HTTP -> GET, POST, PUT , DELETE
 const router = express.Router(); // Detectar verbos http
+const profe = {
+  nombre: "Franco Di Leo",
+};
+const alumnos = [
+  {
+    id: 1,
+    nombre: "Fran",
+    apellido: "Di Leo",
+    edad: 26,
+  },
+  {
+    id: 2,
+    nombre: "Pablo",
+    apellido: "Banfi",
+    edad: 39,
+  },
+];
+router.get("/", (req, res) => {
+  res.render("alumnos", {
+    alumnos: alumnos,
+    profe: profe,
+    curso: "Programador web inicial",
+  });
+});
 
-//Router.use() requires a middleware function but got a Object
 module.exports = router;
